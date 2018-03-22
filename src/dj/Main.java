3f46fb4c6ffdf98a5a228a7e43c89cbbe03ea6a5
Dhananjay <git.medhanu@gmail.com>
2018-03-22 02:36:20 -0500

    java-test code

---
+++ src/dj/Main.java
@@ -0,0 +1,13 @@
+package dj;
+
+import localmath.Ops;
+
+public class Main 
+{
+    public static void main(String[] args) 
+	{
+                int num = Integer.parseInt(args[0]);	
+                Ops m1 = new Ops();
+        	System.out.println("Result is: " + m1.square(num));
+    	}
+}
---
+++ src/localmath/Ops.java
@@ -0,0 +1,10 @@
+package localmath;
+
+public class Ops 
+{
+	    public int square(int number1) 
+	    {
+             	return number1*number1;
+  	    }
+}
+
